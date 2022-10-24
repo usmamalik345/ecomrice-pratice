d6de775a3ca3374e497223978630fb5426620bec
Usama Malik <usmamalik344@gmail.com>
2022-10-24 02:22:54 -0700

    adding-routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'cart',
+    component: CartComponent,
+  },
+  {
+    path: 'contactus',
+    component: ContactComponent,
+  },
+
+  {
+    path: 'home', redirectTo: '/home', pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './Components/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { CartComponent } from './components/cart/cart.component';
 import { HerosComponent } from './components/heros/heros.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CartComponent,
-    HerosComponent
+    HerosComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
